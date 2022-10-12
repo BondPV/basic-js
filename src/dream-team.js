40488b3571f22b572e153d8e8d29a3e571d678a9
BondPV <bondpv@yandex.ru>
2022-10-12 13:01:01 +0300

    feat: dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let nameTeam;
+    let nameTeamBase = [];
+    members.forEach(elem => {
+      if (typeof(elem) === 'string') {
+        nameTeamBase.push(elem.toUpperCase().trim().charAt(0));
+      }
+    });
+    return nameTeamBase.sort().join('');
+  } else {
+    return false;
+  }
+
 }
+
+console.log(
+  createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']),
+  createDreamTeam([' Olivia', 1111, ' Lily', 'Oscar', true, null]),
+  createDreamTeam([
+    ['David Abram'],
+    ['Robin Attfield'],
+    'Thomas Berry',
+    ['Paul R.Ehrlich'],
+    'donna Haraway',
+    ' BrIaN_gOodWiN  ',
+    {
+      0: 'Serenella Iovino'
+    },
+    'Erazim Kohak',
+    '  val_plumwood',
+  ])
+);
+
 
 module.exports = {
   createDreamTeam
